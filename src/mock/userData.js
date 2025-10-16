f6d72c67755e6f02db4632cb6f913b0facbc1e1f
Misak Sofoyan <misaksofoyan@gmail.com>
2025-10-16 13:18:05 +0400

    update

--- src/mock/userData.js
+++ src/mock/userData.js
@@ -2,7 +2,7 @@ const mockUserData = {
     id: 0,
     name: 'Dwayne Johnson',
     balance: 23500,
-    thumbnail: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fhappy-for-thumbnail-young-boy-man-style-look-give-a-thumbs-up_21079506.html&psig=AOvVaw1dXb2pMDhzzDJOV_0p3uSV&ust=1760691859921000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIibt7yuqJADFQAAAAAdAAAAABAE',
+    thumbnail: 'https://png.pngtree.com/png-clipart/20250526/original/pngtree-happy-for-thumbnail-young-boy-man-style-look-give-a-thumbs-png-image_21079506.png',
 };
 
 export default mockUserData;
